ec6e3d52497ac36c4e24fda7542da171e4534a2b
GeorgiSv <georgi.sv.georgiev@mail.bg>
2019-12-21 00:05:22 +0200

    Unnecessary ode removed

---
+++ EpicRapBattlesOfWetas/Models/CharactersNames/Contracts/INames.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EpicRapBattlesOfWetas.Models.CharactersNames.Contracts
+{
+    public interface INames
+    {
+        List<string> GameHeroes { get; }
+
+        List<string> FamousPeople { get; }
+        List<string> MovieCharacters { get; }
+
+    }
+}
---
+++ EpicRapBattlesOfWetas/Models/CharactersNames/MCStarshyNames.cs
@@ -0,0 +1,43 @@
+﻿using EpicRapBattlesOfWetas.Models.CharactersNames.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EpicRapBattlesOfWetas.Models.CharactersNames
+{
+    public class MCStarshyNames : INames
+    {
+        private List<string> gameHeroes = new List<string>
+        {
+            "Kenshi",
+            "Super Mario",
+            "Trevor",
+            "Goro",
+            "",
+        };
+        private List<string> famousPeople = new List<string>
+        {
+            "Roland Atkinson",
+            "Slavi",
+            "Karl Marx",
+            "Spidy",
+        };
+        private List<string> movieCharacters = new List<string>
+        {
+            "Thor",
+            "Dumbledoor",
+            "DeadPool",
+            "Spidy",
+        };
+
+        public List<string> GameHeroes
+            => this.gameHeroes;
+
+        public List<string> FamousPeople
+            => this.famousPeople;
+
+        public List<string> MovieCharacters
+            => this.movieCharacters;
+    }
+}
