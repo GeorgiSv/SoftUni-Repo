f873b06b44934b35511b64859432199f1ab9e5da
GeorgiSv <georgi.sv.georgiev@mail.bg>
2019-12-22 14:12:32 +0200

    JS added as logic // temporary

--- EpicRapBattlesOfWetas/wwwroot/js/site.js
+++ EpicRapBattlesOfWetas/wwwroot/js/site.js
@@ -2,3 +2,108 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+var type = document.getElementById("slect-character");
+var selecteType = type.options[type.selectedIndex].value;
+
+
+//MC Starshey
+
+var MCStarshyMovieNames = [
+    'DeadPool',
+    'Harry Potter',
+    'Dumbledore',
+    'Lucifer',
+    'Michael Scofield ',
+    'Capitan America',
+    'John Snow'
+]
+var MCStarshyFamousNames = [
+    'B. Borisov',
+    'Slavi The Clashers',
+    'Karl Marks',
+    'Donald T.',
+    'Putin',
+]
+
+var MCStarshyGameNames = [
+    'Pikachu',
+    'Spider-man',
+    'El Primo',
+    'Kenshi',
+    'Goro',
+    'Broke',
+    'Trevor',
+    'Hog RIder'
+]
+
+
+game-heroes">Gam
+
+famous-people">F
+movie-characters
+
+if (selecteType == "famous-people") {
+
+    function newCharacterStarshey() {
+
+        var randomNumber = Math.floor(Math.random() * MCStarshyFamousNames.length);
+        document.getElementById('characterDisplayStarshey').innerHTML = MCStarshyFamousNames[randomNumber];
+    }
+}
+else if (selecteType.toString() == "game-heroes") {
+
+    function newCharacterStarshey() {
+
+        var randomNumber = Math.floor(Math.random() * MCStarshyGameNames.length);
+        document.getElementById('characterDisplayStarshey').innerHTML = MCStarshyGameNames[randomNumber];
+    }
+}
+else if (selecteType.toString() == "movie-characters") {
+
+    function newCharacterStarshey() {
+
+        var randomNumber = Math.floor(Math.random() * MCStarshyMovieNames.length);
+        document.getElementById('characterDisplayStarshey').innerHTML = MCStarshyMovieNames[randomNumber];
+    }
+}
+
+//Trendy Di 420
+
+var TrendyNames = [
+
+    'Aragorn ',
+    'Sauron',
+    'Joker',
+    'Anakin Skywalker',
+    'Professor X',
+    'The Flash',
+    'Leonardo(TMNT)',
+    'The accountant',
+    'Jason Bourne',
+    'Dean Winchester',
+
+]
+
+function newCharacterTrendy() {
+
+    var randomNumber = Math.floor(Math.random() * TrendyNames.length);
+    document.getElementById('characterDisplayTrendy').innerHTML = TrendyNames[randomNumber];
+}
+
+//Guttman
+
+var GuttmanNames = [
+    'Iron Man',
+    'Gandalf',
+    'Optimus Prime',
+    'Green Lanthern',
+    'Thorin',
+]
+
+function newCharacterGuttman() {
+
+    var randomNumber = Math.floor(Math.random() * GuttmanNames.length);
+    document.getElementById('characterDisplayGuttman').innerHTML = GuttmanNames[randomNumber];
+}
