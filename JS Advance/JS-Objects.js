ee5dc100a64a3602e8acf4fdb9d62a1836e5b947
GeorgiSv <georgi.sv.georgiev@mail.bg>
2020-02-02 17:46:45 +0200

    JS Objects - Exc Started

--- JS Advance/JS-Objects.js
+++ JS Advance/JS-Objects.js
@@ -1,19 +1,38 @@
-function solveHeroicInvetory(input){
-    let heroes = [];
+// function solveHeroicInvetory(input){
+//     let heroes = [];
 
-    for(let line of input){
-        let [name, level, items] = line.split(" / ");
-        level = Number(level);
-        items = items ? items.split(", ") : [];
+//     for(let line of input){
+//         let [name, level, items] = line.split(" / ");
+//         level = Number(level);
+//         items = items ? items.split(", ") : [];
 
-        heroes.push({name, level, items});
-    }
+//         heroes.push({name, level, items});
+//     }
     
-    console.log(JSON.stringify(heroes));
-}
+//     console.log(JSON.stringify(heroes));
+// }
 
-let test = ['Isacc / 25 / Apple, GravityGun',
-'Derek / 12 / BarrelVest, DestructionSword',
-'Hes / 1 / Desolator, Sentinel, Antara'];
+// let test = ['Isacc / 25 / Apple, GravityGun',
+// 'Derek / 12 / BarrelVest, DestructionSword',
+// 'Hes / 1 / Desolator, Sentinel, Antara'];
 
-solveHeroicInvetory(test);
+// solveHeroicInvetory(test);
+
+// function solveJSONTable(input){
+
+//     //       let arr = JSON.parse(json);
+//     //       let outputArr = ["<table>"];
+//     //       outputArr.push(makeKeyRow(arr));
+//     //       arr.forEach((obj) => outputArr.push(makeValueRow(obj)));
+//     //       outputArr.push("</table>"); 
+//     //       function makeKeyRow(arr) { // ToDo }
+//     //       function makeValueRow(obj) { // ToDo };
+//     //       function escapeHtml(value) { // ToDo };
+//     //       console.log(outputArr.join('\n'));
+        
+// }
+
+// let test = ['{"name":"Pesho","position":"Promenliva","salary":100000}',
+// '{"name":"Teo","position":"Lecturer","salary":1000}',
+// '{"name":"Georgi","position":"Lecturer","salary":1000}'];
+// solveJSONTable(test);
