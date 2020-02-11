2c9ae4e412e69cc6e8b91631d2f52cc01795af3f
GeorgiSv <georgi.sv.georgiev@mail.bg>
2020-02-11 23:59:22 +0200

    DOM Exc 5

--- JS Advance/JS-DOM/05. Order-the-Names/solution.js
+++ JS Advance/JS-DOM/05. Order-the-Names/solution.js
@@ -1,4 +1,34 @@
 function solve() {
+    let button = document.getElementsByTagName("button")[0];
+    let inputField = document.getElementsByTagName("input")[0];
 
-    //TODO...
+    let list = document.getElementsByTagName("ol")[0];
+    let liElements = list.getElementsByTagName("li");
+
+    button.addEventListener("click", function() {
+
+        let inputText = inputField.value;
+
+        if (inputText) {
+            
+            let readyInput = "";
+            readyInput += inputText[0].toUpperCase();
+
+            for (let i = 1; i < inputText.length; i++) {
+                readyInput += inputText[i].toLowerCase();
+            }
+
+            let index = readyInput.charCodeAt(0) - 65;
+
+            if(liElements[index].textContent.length === 0){
+
+                liElements[index].textContent += readyInput;
+            }
+            else{
+                liElements[index].textContent += `, ${readyInput}`;
+            }
+
+            inputField.value = "";
+        }
+    })
 }
