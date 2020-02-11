1ca85e4dc4b3501710d98003bf70f15cb9ae00f4
GeorgiSv <georgi.sv.georgiev@mail.bg>
2020-02-11 23:07:07 +0200

    DOM Exc 4 - Calculator

--- JS Advance/JS-DOM/04. Numpad-Calculator/solution.js
+++ JS Advance/JS-DOM/04. Numpad-Calculator/solution.js
@@ -1,4 +1,49 @@
 function solve() {
 
-    //TODO...
+    let keys = document.getElementsByClassName("keys")[0];
+    let output = document.getElementById("expressionOutput");
+    let clearButton = document.getElementsByClassName("clear")[0];
+    let resultField = document.getElementById("resultOutput");
+
+    clearButton.addEventListener("click", () =>{
+
+        output.innerHTML = "";
+        resultField.innerText = "";
+
+    })
+
+    let operations = {
+        "+": (num1,num2) => Number(num1) + Number(num2),
+        "-": (num1,num2) => Number(num1) - Number(num2),
+        "*": (num1,num2) => Number(num1) * Number(num2),
+        "/": (num1,num2) => Number(num1) / Number(num2),
+    }
+    let operators = ["+","-","/","*"] ; 
+
+    keys.addEventListener("click", (e) => {
+
+        let element = e.target;
+        let value = element.value;
+
+        if(value === "="){
+
+            let [num1, operator, num2] = output.innerHTML.split(" ");
+
+            if(num2){
+                resultField.innerText = operations[operator](num1, num2);
+            }
+            else{
+                resultField.innerText = "NaN";
+            }
+            return;
+        }
+
+        if(operators.includes(value)){
+            
+            output.innerHTML += ` ${value} `;
+            return;
+        }
+        
+        output.innerHTML += value;
+    });
 }
