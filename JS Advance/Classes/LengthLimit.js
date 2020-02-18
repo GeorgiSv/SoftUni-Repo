fbe6562a8b37acde2629a98417862b57d6e53586
GeorgiSv <georgi.sv.georgiev@mail.bg>
2020-02-19 00:15:29 +0200

    JS exc LengthLimit completed

--- JS Advance/Classes/LengthLimit.js
+++ JS Advance/Classes/LengthLimit.js
@@ -1,30 +1,40 @@
 class Stringer{
 
-    initialString;
-
     constructor(string, length){
         this.innerString  = string;
-        this.initialString = string;
         this.innerLength = Number(length);
     }
     
-    get length(){
-        return this.innerLength;
-    }
-    set length(value){
-        if(this.innerLength - value < 0){
-            this.innerLength = 0;
-        }
-    }
     increase(length) {
-        this.length += length;
+        this.innerLength += length;
     }
 
     decrease(length){
-        this.innerLength -= length; 
+        this.innerLength = Math.max(0, this.innerLength - length); 
     }
 
     toString(){
-        this.innerString
+        
+        if (this.innerString.length > this.innerLength) {
+            return this.innerString.substring(0, this.innerLength) + "...";
+        }
+        else if(this.innerString.length <= this.innerLength){
+            return this.innerString;
+        }
+        else{
+            return "...";
+        }
     }
 }
+
+let test = new Stringer("Test", 5);
+console.log(test.toString()); // Test
+
+test.decrease(3);
+console.log(test.toString()); // Te...
+
+test.decrease(5);
+console.log(test.toString()); // ...
+
+test.increase(4); 
+console.log(test.toString()); // Test
