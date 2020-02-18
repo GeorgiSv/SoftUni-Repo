05da864d830d7b3ac00b6977706e13f8bccb7cdb
GeorgiSv <georgi.sv.georgiev@mail.bg>
2020-02-18 23:41:46 +0200

    JS exc DataClass added

---
+++ JS Advance/Classes/DataClass.js
@@ -0,0 +1,14 @@
+class data{
+    constructor(method, uri, version, message){
+    	this.method = method
+    	this.uri = uri; 
+    	this.version = version;
+        this.message = message;
+    	this.response = undefined;
+    	this.fulfilled = false;
+
+    }
+
+    // response, fulfilled
+
+}
