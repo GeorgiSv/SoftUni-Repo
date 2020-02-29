a8b581a17097810ee86c6ca02b0f912ff46854d7
GeorgiSv <georgi.sv.georgiev@mail.bg>
2020-02-29 12:28:01 +0200

    PB Exc 05

---
+++ PB-Exams/01.Exercise/ConsoleApp1/Exc05/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exc05
+{
+    class Program
+    {
+        static void Main()
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int hight = int.Parse(Console.ReadLine());
+
+            int freeSpace = width * length * hight;
+
+            string command = Console.ReadLine(); ;
+            int computersCount = 0;
+
+            while (command != "Done")
+            {
+                if (command == "")
+                {
+                    continue;
+                }
+
+                computersCount += int.Parse(command);
+
+                if (computersCount > freeSpace)
+                {
+                    Console.WriteLine($"No more free space! You need {computersCount - freeSpace} Cubic meters more.");
+                    return;
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{freeSpace - computersCount} Cubic meters left.");
+        }
+    }
+}
