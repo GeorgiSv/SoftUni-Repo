9b3338b86511e1d6bccee8c1dced1ac284b3b725
GeorgiSv <georgi.sv.georgiev@mail.bg>
2020-02-29 12:02:50 +0200

    Pb exam exc 04

---
+++ PB-Exams/01.Exercise/ConsoleApp1/Exc04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exc04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+
+            while (m >= n)
+            {
+                if (m % 2 == 00 && m % 3 == 0)
+                {
+                    if (m == s)
+                    {
+                        return;
+                    }
+                    Console.Write(m + " ");
+                }
+                m--;
+            }
+        }
+    }
+}
