87d72bc0dd02ba90a1bf502fb8246c8088cf1e29
GeorgiSv <georgi.sv.georgiev@mail.bg>
2020-02-29 11:47:02 +0200

    PB exam 01 + 02 exc

---
+++ PB-Exams/01.Exercise/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    using System;
+    public class Program
+    {
+        public static void Main()
+        {
+            double mackerelPriceKg = double.Parse(Console.ReadLine());
+            double cacalPriceKg = double.Parse(Console.ReadLine());
+            double palamudKilos = double.Parse(Console.ReadLine());
+            double safridKilos = double.Parse(Console.ReadLine());
+            double musselsKilos = double.Parse(Console.ReadLine());
+
+            double palamudPriceKg = mackerelPriceKg + mackerelPriceKg * 0.60;
+            double safridPriceKg = cacalPriceKg + cacalPriceKg * 0.80;
+            double musselsPriceKg = 7.50;
+
+            double palamudSum = palamudKilos * palamudPriceKg;
+            double safridlSum = safridKilos * safridPriceKg;
+            double musselsSum = musselsPriceKg * musselsKilos;
+
+            double result = palamudSum + safridlSum + musselsSum;
+            Console.WriteLine($"{result:F2}");
+        }
+    }
+}
---
+++ PB-Exams/01.Exercise/ConsoleApp1/Exc02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exc02
+{
+    class Program
+    {
+        static void Main()
+        {
+            double movieBudget = double.Parse(Console.ReadLine());
+            int statistsCount = int.Parse(Console.ReadLine());
+            double clothePrice = double.Parse(Console.ReadLine());
+
+            double decorationPrice = movieBudget * 0.10;
+
+            if (statistsCount > 150)
+            {
+                clothePrice -= clothePrice * 0.10; 
+            }
+
+            double clothesMoney = clothePrice * statistsCount;
+            double neededMoney = decorationPrice + clothesMoney;
+
+            if (neededMoney > movieBudget)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {(neededMoney - movieBudget):f2} leva more.");
+            }
+            else
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {(movieBudget - neededMoney):f2} leva left.");
+            }
+        }
+    }
+}
