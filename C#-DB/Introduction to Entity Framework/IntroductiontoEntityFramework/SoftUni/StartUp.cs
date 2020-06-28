b6f6eabd32c9f06b03ff0b1a741d68654634cd94
GeorgiSv <georgi.sv.georgiev@mail.bg>
2020-06-28 18:09:39 +0300

    c# db-EF problem 1

---
+++ C#-DB/Introduction to Entity Framework/IntroductiontoEntityFramework/SoftUni/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var db = new SoftUniContext();
+
+            Console.WriteLine(GetEmployeesFullInformation(db));
+
+        }
+
+        //Problem 1
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                    .OrderBy(e => e.EmployeeId)
+                    .Select(e => new
+                    {
+                        FullName = e.FirstName + " " + e.LastName + " " + e.MiddleName,
+                        e.JobTitle,
+                        e.Salary
+                    })
+                    .ToList();
+
+            foreach (var employ in employees)
+            {
+                sb.AppendLine(string.Join(" ", employ.FullName, employ.JobTitle, $"{employ.Salary:F2}"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
