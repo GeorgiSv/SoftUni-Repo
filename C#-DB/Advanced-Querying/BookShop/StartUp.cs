65fb3ce689bc2fba01df2047bb2730c7dc0a703f
GeorgiSv <georgi.sv.georgiev@mail.bg>
2020-07-12 13:16:23 +0300

    db advanced querying #1,#2

---
+++ C#-DB/Advanced-Querying/BookShop/StartUp.cs
@@ -0,0 +1,61 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            //string input = Console.ReadLine();
+
+            var result = GetGoldenBooks(db);
+
+            Console.WriteLine(result);
+        }
+
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var sb = new StringBuilder();
+
+            var books = context
+                .Books
+                .AsEnumerable()
+                .Where(b => b.AgeRestriction.ToString().ToLower() == command.ToLower())
+                .Select(b =>b.Title)
+                .OrderBy(bt=> bt)
+                .ToList();
+
+
+
+            sb.AppendLine(string.Join(Environment.NewLine, books));
+
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var sb = new StringBuilder();
+
+            var bookTitles = context
+                .Books
+                .AsEnumerable()
+                .Where(b => b.EditionType.ToString() == "Gold" && b.Copies < 5000)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToList();
+
+            sb.AppendLine(string.Join(Environment.NewLine, bookTitles));
+
+           return sb.ToString().TrimEnd();
+        }
+    }
+}
